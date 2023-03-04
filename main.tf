92ea3c450f45ae997e96e04fa127c89c523573a8
salonee267 <62054024+salonee267@users.noreply.github.com>
2023-03-04 23:59:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -62,7 +62,7 @@ resource "aws_lambda_function" "test_lambda" {
 #   source_code_hash = filemd5("./lambdademo.zip")  
   filename      = "./lambdademo.zip"  ##give full local path
   function_name = "test-function"
-  role          = "arn:aws:iam::645240902082:role/LambdaRoleForS3andRDS"   ##iam role -- with permission to lambda, s3, rds
+  role          = "arn:aws:iam::645240902082:role/LambdaRoleForS3andRDS"   ##Lambda exec role -- with permission for lambda to access s3, rds
   handler       = "lambdademo.lambda_handler"
   runtime       = "python3.7"
 
