cbe108ce3aeaf44d4c86c078d71a9edb1cdd08e6
salonee267 <62054024+salonee267@users.noreply.github.com>
2023-03-07 23:29:10 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -6,3 +6,16 @@ resource "aws_s3_bucket" "lep_demo" {
      enabled = true
    }
 }
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.lep_demo.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.source.arn
+    events              = ["s3:ObjectCreated:*"]
+    filter_prefix       = ""
+    filter_suffix       = ".csv"
+  }
+
+  depends_on = [aws_lambda_permission.allow_bucket]
+}
