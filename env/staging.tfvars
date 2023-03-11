fe6046f9974c819ce05a8d95ec12430fb5d69400
salonee267 <salonee267@gmail.com>
2023-03-11 23:20:23 +0530

    referencing to tfvars for each env from default

---
+++ env/staging.tfvars
@@ -0,0 +1,7 @@
+identifier = "test_db_staging"
+allocated_storage = 10
+db_name = "db_name_staging"
+username = "admin"
+password = "password"
+bucket_name = "lep-demo-s3-bucket-773"
+lambda_role = "arn:aws:iam::645240902082:role/LambdaRoleForS3andRDS"
--- variables.tf
+++ variables.tf
@@ -1,44 +1,44 @@
 variable "identifier" {
   description = "name of database identifier- this name will reflect in AWS console"
   type        = string
-  default     = "testdb"
+  # default     = "testdb"
 }
 
 variable "allocated_storage" {
   description = "allocated storage for the db"
   type        = number
-  default     = 10
+  # default     = 10
 }
 
 variable "db_name" {
   description = "The name of the database to create when the DB instance is created"
   type        = string
-  default     = "testdb"
+  # default     = "testdb"
 }
 
 variable "username" {
   description = "Username for the DB user"
   type        = string
-  default     = "admin"
+  # default     = "admin"
 }
 
 variable "password" {
   description = "Password for the DB user"
   type        = string
-  default     = "password"
+  # default     = "password"
 }
 
 variable "bucket_name" {
   description = "name of the bucket"
   type        = string
-  default     = "lep-demo-s3-bucket-12356784"
+  # default     = "lep-demo-s3-bucket-12356784"
 }
 
 variable "lambda_role" {
   description = "arn of the lambda role for S3 and RDS"
   type        = string
 #   default     = "arn:aws:iam::645240902082:role/LambdaRoleForS3andRDS"
-  default     = "arn:aws:iam::058399968204:role/LambdaRoleForS3andRDS_prod"
+  # default     = "arn:aws:iam::058399968204:role/LambdaRoleForS3andRDS_prod"
 }
 
 
