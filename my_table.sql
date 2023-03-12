502befe3f7743631b68ac423b95719c8775533d5
salonee267 <salonee267@gmail.com>
2023-03-12 16:22:56 +0530

    fixing syntax- mysql

--- my_table.sql
+++ my_table.sql
@@ -1,6 +1,6 @@
-USE "${db_name}";
+USE '${db_name}';
 
-CREATE TABLE "${table_name}" (
+CREATE TABLE '${table_name}' (
   id INT NOT NULL AUTO_INCREMENT,
   email VARCHAR(50) NOT NULL,
   PRIMARY KEY (id)
