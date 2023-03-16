eace4acc281e7028d43bdf20e745c19fa2ceb6c2
salonee267 <salonee267@gmail.com>
2023-03-16 16:16:20 +0530

    added sse encryption for s3 security

--- s3.tf
+++ s3.tf
@@ -2,6 +2,14 @@ resource "aws_s3_bucket" "lep_demo" {
    bucket = var.bucket_name
    acl    = "private"
 
+   server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
    versioning {
      enabled = true
    }
@@ -19,3 +27,29 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
 
   depends_on = [aws_lambda_permission.allow_bucket]
 }
+
+data "aws_iam_policy_document" "s3_sse_policy" {
+  statement {
+    actions   = ["s3:PutObject"]
+    # resources = ["arn:aws:s3:::my-s3-bucket/*"]
+    resources = var.bucket_name
+    condition {
+      test     = "StringEquals"
+      variable = "s3:x-amz-server-side-encryption"
+      values   = ["AES256"]
+    }
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = var.bucket_name
+
+  policy = data.aws_iam_policy_document.s3_sse_policy.json
+}
+
+
